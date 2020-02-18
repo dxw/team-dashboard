ea6e6cd55084a5ba81ce2c4bff43c4def162b7df
Lorna <lorna@dxw.com>
2020-02-18 16:00:56 +0000

    Test the associations between Projects and TeamMembers
    Shows and tests the belong_to relationship of project to a team member and the has_many relationship
    of team members to project.
    
    We have left the belong_to relationship which only allows one project for a team member as
    we feel this association is enough for our first pass. We will change this to a many to many relationship
    at a later date.
    
    Co-authored-by: Olivia <olivia@dxw.com>

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe Project, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
+    it { should have_many(:team_members) }
   end
 end
--- spec/models/team_member_spec.rb
+++ spec/models/team_member_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe TeamMember, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:tenk_id) }
+    it { should belong_to(:project) }
   end
 end
