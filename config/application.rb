a69590ab35d24785d768beccc07a58b4fc691323
Dominic Baggott <dominic.baggott@gmail.com>
2020-03-06 17:56:33 +0000

    Allow for an X-Frame-Options header
    This is to allow us to whitelist our dashboard screen orchestrator, so
    it can load this dashboard in an iframe.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module TeamDashboard
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+
+    if ENV['IFRAME_ALLOW_ORIGIN'].present?
+      config.action_dispatch.default_headers.merge!(
+        'X-Frame-Options' => "Allow-From #{ENV['IFRAME_ALLOW_ORIGIN']}",
+      )
+    end
   end
 end
