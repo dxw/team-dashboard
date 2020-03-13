1fbf66dec6c30050f0b56a34973659d07b3bf3b7
Lorna <lorna@dxw.com>
2020-03-13 15:47:42 +0000

    Projects can now have a tenk_id field

--- spec/features/user_can_see_a_team_member_in_a_project_spec.rb
+++ spec/features/user_can_see_a_team_member_in_a_project_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'user can see team members within a project', type: 'feature' do
   scenario 'they can see one team member within a given project' do
-    dashboard = Project.create(name: 'Dashboard')
+    dashboard = Project.create(name: 'Dashboard', tenk_id: 1234)
     member = TeamMember.create(first_name: 'Joe', last_name: "Smith", project: dashboard, tenk_id: 1234)
 
     visit '/'
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe Project, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:tenk_id) }
     it { should have_many(:team_members) }
   end
 end
